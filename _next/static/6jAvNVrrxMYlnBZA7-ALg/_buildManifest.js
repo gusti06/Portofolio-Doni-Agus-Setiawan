853bcfae5fcf2870dd7d75fcf5da9118d0297f6a
gusti06 <gusti06@users.noreply.github.com>
2026-05-14 16:18:30 +0000

    deploy: c37886d24d4bed13840207f6268b4a01952d8ad6

---
+++ _next/static/6jAvNVrrxMYlnBZA7-ALg/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0dsjjjeefvpo4.js"
+  ],
+  "/_error": [
+    "static/chunks/0c-2w_qe~mmxx.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/0dsjjjeefvpo4.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/0yk87~b5zv3i2.js",
+  "static/chunks/01v.giln3xmdc.js",
+  "static/chunks/0itjoznayg1x4.js",
+  "static/chunks/12vdghcfj.qcj.js",
+  "static/chunks/turbopack-0bp1-0bj7lvf0.js"
+])
